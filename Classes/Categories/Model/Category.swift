19d6ae51a4748de4c246cf657057aef6793e6f74
florianldt <florian.ludot@gmail.com>
2019-08-07 20:21:16 +0900

    create Category model

---
+++ Classes/Categories/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  GardinerNotebook
+//
+//  Created by Florian LUDOT on 8/7/19.
+//  Copyright © 2019 Florian LUDOT. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    let id: String
+    let name: String
+}
