9e645295f6c6979299363c6bb9f1f47497767418
florianldt <florian.ludot@gmail.com>
2019-08-07 20:16:13 +0900

    add CategoriesViewController + rooted

---
+++ Classes/Categories/CategoriesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesViewController.swift
+//  GardinerNotebook
+//
+//  Created by Florian LUDOT on 8/7/19.
+//  Copyright © 2019 Florian LUDOT. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Categories"
+        view.backgroundColor = .white
+    }
+}
