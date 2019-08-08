fecab1b0acbac7851e51eba912c057dee51be166
florianldt <florian.ludot@gmail.com>
2019-08-08 18:46:57 +0900

    add UITableView + UICollectionView extensions

---
+++ Classes/Helpers/UITableView+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UITableView+Extension.swift
+//  GardinerNotebook
+//
+//  Created by Florian LUDOT on 8/8/19.
+//  Copyright © 2019 Florian LUDOT. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    func register<T: UITableViewCell>(_: T.Type, reuseIdentifier: String? = nil) {
+        self.register(T.self, forCellReuseIdentifier: reuseIdentifier ?? String(describing: T.self))
+    }
+
+    func dequeue<T: UITableViewCell>(_: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as? T else { fatalError("Could not deque cell with type \(T.self)") }
+
+        return cell
+    }
+}
+
+
