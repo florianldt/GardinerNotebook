a25f20b6b0e95fd869299c5df7a62d669310ac44
florianldt <florian.ludot@gmail.com>
2019-08-10 21:57:18 +0900

    Create String+Extension.swift

---
+++ Classes/Helpers/String+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  String+Extension.swift
+//  GardinerNotebook
+//
+//  Created by Florian LUDOT on 8/10/19.
+//  Copyright © 2019 Florian LUDOT. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+
+    func or(_ substitute: String) -> String {
+        return self.isEmpty ? substitute : self
+    }
+    
+    func heightWithConstrainedWidth(_ width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: CGFloat.greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect,
+                                            options: NSStringDrawingOptions.usesLineFragmentOrigin,
+                                            attributes: [NSAttributedString.Key.font: font],
+                                            context: nil)
+        return boundingBox.height
+    }
+}
