8bb0e4ce885e58e3e72dec9263bfcd7e9a9e138c
florianldt <florian.ludot@gmail.com>
2019-08-10 21:57:13 +0900

    Create Consts.swift

---
+++ Classes/Helpers/Consts.swift
@@ -0,0 +1,17 @@
+//
+//  Consts.swift
+//  GardinerNotebook
+//
+//  Created by Florian LUDOT on 8/10/19.
+//  Copyright © 2019 Florian LUDOT. All rights reserved.
+//
+
+import Foundation
+
+enum Consts {
+
+    enum Urls {
+
+        static let github = URL(string: "https://github.com/florianldt")!
+    }
+}
