5ffccfea90e00ba8e203ee7ad99bf45d62930dfb
florianldt <florian.ludot@gmail.com>
2019-08-22 20:24:28 +0900

    change tabbar tint color to gold

--- Classes/Supporting Files/AppDelegate.swift
+++ Classes/Supporting Files/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         UINavigationBar.appearance().tintColor = .black
-        UITabBar.appearance().tintColor = .black
+        UITabBar.appearance().tintColor = UIColor.fromHex(Styles.Colors.gold)
         UIBarButtonItem.appearance().tintColor = .black
 
         FirebaseApp.configure()
